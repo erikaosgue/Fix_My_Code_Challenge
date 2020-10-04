b9aa6d329cb7fb829ba1bb3f11c93116896099a5
erikaosgue <1476@holbertonschool.com>
2020-10-04 03:54:50 +0000

    Add task 1: My square

---
+++ 0x01-challenge/square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+class square():
+
+    size = 0
+    height = 0
+
+    def __init__(self, *args, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def area_of_my_square(self):
+        """ Area of the square """
+        return self.size * self.size
+
+    def PermiterOfMySquare(self):
+        return (self.size * 2) + (self.size * 2)
+
+    def __str__(self):
+        return "{}/{}".format(self.size, self.size)
+
+
+if __name__ == "__main__":
+
+    s = square(size=12)
+    print(s)
+    print(s.area_of_my_square())
+    print(s.PermiterOfMySquare())
