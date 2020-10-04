afde173cc1aeff6f1100582fb3b100ddd32e413e
erikaosgue <1476@holbertonschool.com>
2020-10-04 23:17:50 +0000

    Add Documentation

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Module squeare, defines a class Square """
 
+
 class Square():
     """ A class square """
 
@@ -16,7 +17,7 @@ class Square():
         """ Area of the square """
         return self.width * self.height
 
-    def PermiterOfMySquare(self):
+    def perimiter_of_my_square(self):
         """ Perimeter of the square """
         return (self.width * 2) + (self.height * 2)
 
@@ -27,7 +28,7 @@ class Square():
 
 if __name__ == "__main__":
 
-    s = Square(width=12, height=12)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.perimiter_of_my_square())
